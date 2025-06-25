c4734c3134f16844a36592117fd3c4e2fb66f074
Mateo508 <131176063+Mateo508@users.noreply.github.com>
2025-06-25 16:22:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ const architects = {
     }
 };
         
-const questions = {
+const questions = [
     text: 'What kind of materials get you excited to design with?',
     options: [
         { text: 'Sleek steel curves that scream "future"', scores: { hadid: 1, foster: 1 } },
